ef238dced7dfa8fd899470bfbb7ed4a7254806f7
CeliaLaurent <claurent@inogs.it>
2020-02-10 18:41:58 +0100

    add function2 y=exp(x).

--- script.py
+++ script.py
@@ -1,10 +1,12 @@
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
+import math
 
 def usage():
     print("Usage: this scripts takes one <int> argument and plots different functions:")
     print(" - 'python script.py 1' plots y=x")
+    print(" - 'python script.py 2' plots y=exp(x)")
 
 xval=list(np.around(np.arange(-5.,5.,0.1),decimals=1))
 
@@ -16,6 +18,8 @@ fnum=sys.argv[1]
 
 if(fnum=="1"):
     yval = list(map(lambda x: x , xval))
+elif(fnum=="2"):
+    yval = list(map(lambda x: math.exp(x) , xval))
 else:
     usage()
     sys.exit()
