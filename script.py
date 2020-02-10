7684d379d97acb37bf47b5410bdb2f3449e11077
CeliaLaurent <claurent@inogs.it>
2020-02-10 18:39:45 +0100

    add support for printing a 'Usage: ...' message

--- script.py
+++ script.py
@@ -2,17 +2,23 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 
+def usage():
+    print("Usage: this scripts takes one <int> argument and plots different functions:")
+    print(" - 'python script.py 1' plots y=x")
+
 xval=list(np.around(np.arange(-5.,5.,0.1),decimals=1))
 
 if(len(sys.argv)!=2): 
+    usage()
     sys.exit()
 
 fnum=sys.argv[1]
 
 if(fnum=="1"):
-  yval = list(map(lambda x: x , xval))
+    yval = list(map(lambda x: x , xval))
 else:
-  sys.exit()
+    usage()
+    sys.exit()
 
 plt.plot(xval,yval)
 plt.show()
