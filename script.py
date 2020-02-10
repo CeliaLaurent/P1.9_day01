c31e850fff8715139db5dd1e6c26697df42b3407
CeliaLaurent <claurent@inogs.it>
2020-02-10 18:43:11 +0100

    add function3 y=sqrt(|x|).

--- script.py
+++ script.py
@@ -7,6 +7,7 @@ def usage():
     print("Usage: this scripts takes one <int> argument and plots different functions:")
     print(" - 'python script.py 1' plots y=x")
     print(" - 'python script.py 2' plots y=exp(x)")
+    print(" - 'python script.py 3' plots y=sqrt(|x|)")
 
 xval=list(np.around(np.arange(-5.,5.,0.1),decimals=1))
 
@@ -20,6 +21,8 @@ if(fnum=="1"):
     yval = list(map(lambda x: x , xval))
 elif(fnum=="2"):
     yval = list(map(lambda x: math.exp(x) , xval))
+elif(fnum=="3"):
+    yval = list(map(lambda x: math.sqrt(abs(x)) , xval))
 else:
     usage()
     sys.exit()
